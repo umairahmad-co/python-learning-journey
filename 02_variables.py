8edfdae10b05f47c41a49232318948a39d4c2943
umairahmad-co <umairahmad.co@gmail.com>
2026-03-09 19:49:21 +0500

    02_variables

--- 02_variables.py
+++ 02_variables.py
@@ -17,16 +17,16 @@ person_info = {
 
 # Printing the values stored in the variables
 
-print('First name:', first_name)
+print('First name :', first_name)
 print('First name length:', len(first_name))
-print('Last name: ', last_name)
+print('Last name : ', last_name)
 print('Last name length: ', len(last_name))
 print('Country: ', country)
 print('City: ', city)
 print('Age: ', age)
-print('Married: ', is_married)
-print('Skills: ', skills[2])
-print('Person information: ', person_info)
+print('Married : ', is_married)
+print('Skills : ', skills[2])
+print('Person information : ', person_info)
 
 # Declaring multiple variables in one line
 
