85ebe00f3de0b2c6691ade7d7b9ab1db9b356ab4
umairahmad-co <umairahmad.co@gmail.com>
2026-04-01 14:39:23 +0500

    practice.py

---
+++ practice.py
@@ -0,0 +1,14 @@
+first_name='UMAIR'
+last_name='AHMAD'
+space=' '
+
+print(first_name+space+last_name)
+
+sentence="MY NAME IS UMAIR AHMAD\nI HAVE DONE BSCS FROM SUPERIOR UNIVERSITY"
+
+print(sentence)
+print(len(sentence))
+print(type(sentence))
+name=input("Enter your Name : ")
+print(f"Your Name is :",name)
+
