f9da1be6d4438a0b3a02a48eb6a44377a5cc514a
umairahmad-co <umairahmad.co@gmail.com>
2026-06-01 19:34:38 +0500

    update

--- practice.py
+++ practice.py
@@ -1,14 +1,76 @@
-first_name='UMAIR'
-last_name='AHMAD'
-space=' '
+# first_name='UMAIR'
+# last_name='AHMAD'
+# space=' '
 
-print(first_name+space+last_name)
+# print(first_name+space+last_name)
 
-sentence="MY NAME IS UMAIR AHMAD\nI HAVE DONE BSCS FROM SUPERIOR UNIVERSITY"
+# sentence="MY NAME IS UMAIR AHMAD\nI HAVE DONE BSCS FROM SUPERIOR UNIVERSITY"
 
-print(sentence)
-print(len(sentence))
-print(type(sentence))
-name=input("Enter your Name : ")
-print(f"Your Name is :",name)
+# print(sentence)
+# print(len(sentence))
+# print(type(sentence))
+# name=input("Enter your Name : ")
+# print(f"Your Name is :",name)
 
+
+# course='python'
+
+# a,s,d,f,g,h=course
+
+# print(a)
+# print(s)
+# print(d)
+# print(f)
+# print(g)
+# print(h)
+
+# print(a+s+d+f+g+h)
+
+name='UMAIR'
+
+# frist_latter=name[0]
+# print(frist_latter)
+
+# second_latter=name[1]
+# print(second_latter)
+
+# name = 'UMAIR'
+# # first_letter = language[0]
+# # print(first_letter)  # P
+# # second_letter = language[1]
+# # print(second_letter)  # y
+
+# last_index = len(name) - 1
+# last_letter =name[last_index]
+# print(last_letter) 
+
+
+courseName='ComputerScience'
+print(courseName.capitalize())
+print(len(courseName))
+f2=courseName[0:2]
+print(f2)
+f5=courseName[-5]
+print(f5)
+f7=courseName[0:7]
+print(f7)
+f8=courseName[0:8]
+print(f8)
+
+f4=courseName[5:8]
+print(f4)
+print("slice")
+f3=courseName[0:6:2]
+print(f3)
+
+# Escape sequence
+print('I hope every one enjoying the python challenge.\nDo you ?')  # line break
+print('Days\tTopics\tExercises')
+print('Day 1\t3\t5')
+print('Day 2\t3\t5')
+print('Day 3\t3\t5')
+print('Day 4\t3\t5')
+print('This is a back slash  symbol (\\)')  # To write a back slash
+print('In every programming language it starts with \"Hello, World!\"')
+
+print(courseName.count('c'))
